1facd159b0bbc3b881dfefb1fb7ed3e5766fdc6d
sonali <sonali@fixstars.com>
2023-10-05 17:50:34 -0700

    version bump 1.78.0

--- recipes/gobject-introspection/all/conanfile.py
+++ recipes/gobject-introspection/all/conanfile.py
@@ -1,10 +1,18 @@
-from conans import ConanFile, tools, Meson, VisualStudioBuildEnvironment
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.tools.meson import Meson, MesonToolchain
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.layout import basic_layout
+from conan.tools.build import cross_building
+from conan.tools.apple import fix_apple_shared_install_name, is_apple_os
+from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
+from conan.tools.files import apply_conandata_patches, chdir,  export_conandata_patches, get, rename, rm, rmdir,replace_in_file,copy
+from conan.tools.gnu import PkgConfigDeps, PkgConfig
+from conan.tools.scm import Version
 import os
-import shutil
 import glob
 
-required_conan_version = ">=1.36.0"
+required_conan_version = ">=1.56.0 <2 || >=2.0.6"
 
 class GobjectIntrospectionConan(ConanFile):
     name = "gobject-introspection"
@@ -14,102 +22,138 @@ class GobjectIntrospectionConan(ConanFile):
     homepage = "https://gitlab.gnome.org/GNOME/gobject-introspection"
     license = "LGPL-2.1"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
-    generators = "pkg_config"
+    options = {
+        "shared": [True, False], 
+        "fPIC": [True, False],
+        "with_cairo":[True, False],
+        }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "with_cairo": False,
+        }
 
-    @property
-    def _is_msvc(self):
-        return self.settings.compiler == "Visual Studio"
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
+        if self.options.shared:
+            self.options["glib"].shared = True
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-        if self.settings.os == "Windows":
-            raise ConanInvalidConfiguration("%s recipe does not support windows. Contributions are welcome!" % self.name)
+        # if self.settings.os == "Windows":
+        #     raise ConanInvalidConfiguration("%s recipe does not support windows. Contributions are welcome!" % self.name)
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def build_requirements(self):
-        if tools.Version(self.version) >= "1.71.0":
-            self.build_requires("meson/0.62.2")
+        if Version(self.version) >= "1.71.0":
+            self.tool_requires("meson/1.2.2")
         else:
             # https://gitlab.gnome.org/GNOME/gobject-introspection/-/issues/414
-            self.build_requires("meson/0.59.3")
-        self.build_requires("pkgconf/1.7.4")
+            self.tool_requires("meson/0.59.3")
+        self.tool_requires("pkgconf/2.0.3")
         if self.settings.os == "Windows":
-            self.build_requires("winflexbison/2.5.24")
+            self.tool_requires("winflexbison/2.5.25")
         else:
-            self.build_requires("flex/2.6.4")
-            self.build_requires("bison/3.7.6")
+            self.tool_requires("flex/2.6.4")
+            self.tool_requires("bison/3.8.2")
 
     def requirements(self):
-        self.requires("glib/2.73.0")
+        # glib-object.h and gio/gio.h are used in several public headers
+        self.requires("glib/2.78.0", transitive_headers=True)
+        self.requires("libffi/3.4.4", transitive_headers=True)
+        if self.options.with_cairo:
+            self.requires("cairo/1.17.6")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def _configure_meson(self):
-        meson = Meson(self)
-        defs = dict()
-        defs["build_introspection_data"] = self.options["glib"].shared
-        defs["datadir"] = os.path.join(self.package_folder, "res")
+    def generate(self):
+        env = VirtualBuildEnv(self)
+       
+        env.generate()
+        if not cross_building(self):
+            env = VirtualRunEnv(self)
+            env.generate(scope="build")
 
-        meson.configure(
-            source_folder=self._source_subfolder,
-            args=["--wrap-mode=nofallback"],
-            build_folder=self._build_subfolder,
-            defs=defs,
-        )
-        return meson
+        deps = PkgConfigDeps(self)
+        deps.generate()
+
+        tc = MesonToolchain(self)
+        tc.project_options["cairo"] = "enabled" if self.options.with_cairo else "disabled"
+        tc.project_options["build_introspection_data"] = self.dependencies["glib"].options.shared       
+        tc.project_options["datadir"] = os.path.join(self.package_folder, "res")
+        tc.generate()
+
+   
+
+    def validate(self):
+        if is_msvc_static_runtime(self):
+            raise ConanInvalidConfiguration("Static runtime is not supported on Windows due to GLib issues")
+        if self.options.shared and not self.dependencies["glib"].options.shared:
+            raise ConanInvalidConfiguration("Shared gobject-introspection cannot link to static GLib")
+        if is_apple_os(self) and self.dependencies["glib"].options.shared:
+            raise ConanInvalidConfiguration(
+                "macOS builds are disabled when glib is shared until "
+                "conan-io/conan#7324 gets merged to fix macOS SIP issue #8443"
+            )
 
     def build(self):
-        tools.replace_in_file(
-            os.path.join(self._source_subfolder, "meson.build"),
+        apply_conandata_patches(self)
+        replace_in_file(
+            self,
+            os.path.join(self.source_folder, "meson.build"),
             "subdir('tests')",
             "#subdir('tests')",
         )
-        tools.replace_in_file(
-            os.path.join(self._source_subfolder, "meson.build"),
-            "if meson.version().version_compare('>=0.54.0')",
-            "if false",
-        )
+        
+        if is_msvc(self):
+            self.buildenv.append_path("PKG_CONFIG_PATH",self.generators_folder)
+        meson = Meson(self)
+        meson.configure()
+        meson.build()
 
-        with tools.environment_append(
-            VisualStudioBuildEnvironment(self).vars
-            if self._is_msvc
-            else {"PKG_CONFIG_PATH": self.build_folder}
-        ):
-            meson = self._configure_meson()
-            meson.build()
+    def _fix_library_names(self, path):
+        # https://github.com/mesonbuild/meson/issues/1412
+        if not self.options.shared and is_msvc(self):
+            with chdir(self, path):
+                for filename_old in glob.glob("*.a"):
+                    filename_new = filename_old[3:-2] + ".lib"
+                    self.output.info(f"rename {filename_old} into {filename_new}")
+                    rename(self, filename_old, filename_new)
+
 
     def package(self):
-        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-        with tools.environment_append(
-            VisualStudioBuildEnvironment(self).vars
-        ) if self._is_msvc else tools.no_op():
-            meson = self._configure_meson()
-            meson.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
-        for pdb_file in glob.glob(os.path.join(self.package_folder, "bin", "*.pdb")):
-            os.unlink(pdb_file)
+        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        meson = Meson(self)
+        meson.install()
+        self._fix_library_names(os.path.join(self.package_folder, "lib"))   
+
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rm(self, "*.pdb", self.package_folder, recursive=True)
+        fix_apple_shared_install_name(self)
 
     def package_info(self):
-        self.cpp_info.names["pkg_config"] = "gobject-introspection-1.0"
         self.cpp_info.libs = ["girepository-1.0"]
+
         self.cpp_info.includedirs.append(
             os.path.join("include", "gobject-introspection-1.0")
         )
+        self.cpp_info.libdirs = ['lib']  
+        self.cpp_info.bindirs = ['bin'] 
+        self.cpp_info.set_property("pkg_config_name", "gobject-introspection-1.0")
 
+        # self.cpp_info.names["pkg_config"] = "gobject-introspection-1.0"
         bin_path = os.path.join(self.package_folder, "bin")
-        self.output.info("Appending PATH env var with: {}".format(bin_path))
+        self.output.info("Appending PATH environment variable: {}".format(bin_path))
         self.env_info.PATH.append(bin_path)
 
         exe_ext = ".exe" if self.settings.os == "Windows" else ""
--- recipes/gobject-introspection/all/test_package/conanfile.py
+++ recipes/gobject-introspection/all/test_package/conanfile.py
@@ -1,26 +1,54 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.build import can_run,cross_building
+from conan.tools.cmake import CMake, cmake_layout,CMakeToolchain,CMakeDeps
+from conan.tools.gnu import PkgConfig,PkgConfigDeps
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
 import os
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package", "pkg_config"
+    settings = "os", "arch", "compiler", "build_type"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def build_requirements(self):
+        if self.settings.os != "Windows" and not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/2.0.3")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        virtual_run_env = VirtualRunEnv(self)
+        virtual_run_env.generate()   
+
+        if self.settings.os == "Windows":
+            deps = CMakeDeps(self)
+            deps.generate()
+        else:
+            virtual_build_env = VirtualBuildEnv(self)
+            virtual_build_env.generate()
+            pkg_config_deps = PkgConfigDeps(self)
+            pkg_config_deps.generate()
 
     def build(self):
         if self.settings.os != 'Windows':
-            with tools.environment_append({'PKG_CONFIG_PATH': "."}):
-                pkg_config = tools.PkgConfig("gobject-introspection-1.0")
-                for tool in ["g_ir_compiler", "g_ir_generate", "g_ir_scanner"]:
-                    self.run('%s --version' % pkg_config.variables[tool], run_environment=True)
-                self.run('g-ir-annotation-tool --version', run_environment=True)
-                self.run('g-ir-inspect -h', run_environment=True)
-
+            pkg_config = PkgConfig(self, "gobject-introspection-1.0", pkg_config_path=self.generators_folder)
+            for tool in ["g_ir_compiler", "g_ir_generate", "g_ir_scanner"]:
+                self.run('%s --version' % pkg_config.variables[tool], env="conanrun")
+            self.run('g-ir-annotation-tool --version',env="conanrun")
+            self.run('g-ir-inspect -h', env="conanrun")   
+    
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if cross_building(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
 
